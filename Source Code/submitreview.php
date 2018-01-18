7c74389b1e8d860030302f056781b84f7934318c
Parker Daudt <daudt.parker@gmail.com>
2018-01-18 16:08:54 +0000

    Update submitreview.php

--- Source Code/submitreview.php
+++ Source Code/submitreview.php
@@ -10,8 +10,8 @@ else
 }
 
 $host = '127.0.0.1';
-$user = 'root';
-$pass = 'charchar';
+$user = 'websecurity';
+$pass = 'websecurity';
 $db = 'accounts';
 $mysqli = new mysqli($host, $user, $pass, $db) or die($mysqli->error);
 
