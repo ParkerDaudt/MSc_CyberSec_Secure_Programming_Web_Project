e87748bf97dcb282da07d326d7117c1ae71b4707
Parker Daudt <daudt.parker@gmail.com>
2018-01-18 16:08:02 +0000

    Update submitregister.php

--- Source Code/submitregister.php
+++ Source Code/submitregister.php
@@ -12,8 +12,8 @@ else
 }
 
 $host = '127.0.0.1';
-$user = 'root';
-$pass = 'charchar';
+$user = 'websecurity';
+$pass = 'websecurity';
 $db = 'accounts';
 $mysqli = new mysqli($host, $user, $pass, $db) or die($mysqli->error);
 
